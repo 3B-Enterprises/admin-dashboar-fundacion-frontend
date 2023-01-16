42f99689229a61247f21401527b2ec163c4cf7d3
Andrés Prieto <pipeprieto0902@gmail.com>
2023-01-16 13:43:51 -0500

    Update: Loader added

--- fundacionapp/src/pages/Dashboard/Dashboard.js
+++ fundacionapp/src/pages/Dashboard/Dashboard.js
@@ -1,34 +1,56 @@
-import { data } from 'autoprefixer';
 import {useEffect,useState} from 'react'
 import { CardInfo } from '../../components/CardInfo/CardInfo';
 import { Navbar } from '../../components/Nav/Navbar'
 import { Table } from '../../components/Table/Table';
 import {getChildren} from '../../utils/requests'
+import { Loading } from '../Loading/Loading';
 
 export const Dashboard = () => {
   const [data,setData] = useState([])
+  const [loader,setLoader] = useState(false);
   const getUsers = async()=>{
+    setLoader(true)
     const resp = await getChildren();
     setData(resp.data)
+    setLoader(false);
   }
 
   useEffect(()=>{
     getUsers()
   },[])
 
-  return (
-    <div className="flex">
-      <div className="h-screen w-1/6 lg:w-[13%] md:w-[22%] inset-0 z-50">
-        <Navbar />
-      </div>
-      <div className="flex-1">
-        <div className="w-4/3">
-          <CardInfo total={data.length} />
+  if (loader) {
+    return (
+      <div className="flex">
+        <div className="h-screen w-1/6 lg:w-[13%] md:w-[22%] inset-0 z-50">
+          <Navbar />
         </div>
-        <div className="">
-          <Table data={data} />
+        <div className="flex-1">
+          <div className="w-4/3">
+            <CardInfo total={data.length} />
+          </div>
+          <div className="">
+            <Loading/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }else{
+    return (
+      <div className="flex">
+        <div className="h-screen w-1/6 lg:w-[13%] md:w-[22%] inset-0 z-50">
+          <Navbar />
+        </div>
+        <div className="flex-1">
+          <div className="w-4/3">
+            <CardInfo total={data.length} />
+          </div>
+          <div className="">
+            <Table data={data} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
 }
